b376a18c996e76842cfed6af3e626ffae5ca5923
Oanh Nguyen <myspamvn@yahoo.com>
2023-04-24 23:48:03 +0900

    feat: add techstacks, refactor by looping through array of stacks

--- src/components/TechStacks.vue
+++ src/components/TechStacks.vue
@@ -1,32 +1,40 @@
 <template>
-    <div class="font-mono">
-        <div class="flex"> <!-- backend -->
-            <div class="bg-orange-400 rounded m-0.5 p-2 flex justify-center text-xs font-medium">
-                ruby on rails
-            </div>
-            <div class="bg-orange-300 rounded m-0.5 p-2 flex justify-center text-xs font-medium">
-                python
-            </div>
-            <div class="bg-orange-200 rounded m-0.5 p-2 flex justify-center text-xs font-medium">
-                c#
-            </div>
-        </div>
-        <div class="flex"> <!-- frontend -->
-            <div class="bg-black rounded m-0.5 p-2 flex justify-center text-xs text-white font-medium">
-                javascript
-            </div>
-            <div class="bg-slate-300 rounded m-0.5 p-2 flex justify-center text-xs font-medium">
-                typescript
-            </div>
-            <div class="bg-slate-700 rounded m-0.5 p-2 flex justify-center text-xs text-white font-medium">
-                vue.js
-            </div>
-            <div class="bg-slate-700 rounded m-0.5 p-2 flex justify-center text-xs text-white font-medium">
-                bootstrap
-            </div>
-            <div class="bg-slate-400 rounded m-0.5 p-2 flex justify-center text-xs font-medium">
-                tailwind
-            </div>
-        </div>
+  <div class="font-mono">
+    <div class="flex"> <!-- backend -->
+      <div v-for="(stack, i) in backend" :key="i" :class="'bg-' + stack.color + ' rounded m-0.5 p-2 flex justify-center text-xs font-medium'">
+        {{ stack.name }}
+      </div>
     </div>
+    <div class="flex"> <!-- frontend -->
+      <div v-for="(stack, i) in frontend" :key="i" :class="'bg-' + stack.color + ' rounded m-0.5 p-2 flex justify-center text-xs ' + stack.textColor + ' font-medium'">
+        {{ stack.name }}
+      </div>
+    </div>
+    <div class="flex"> <!-- frontend -->
+      <div v-for="(stack, i) in database" :key="i" :class="'bg-' + stack.color + ' rounded m-0.5 p-2 flex justify-center text-xs ' + stack.textColor + ' font-medium'">
+        {{ stack.name }}
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+// import { Ref } from 'vue';
+const backend = [
+  {name: 'ruby on rails', color: 'orange-400'},
+  {name: 'python', color: 'orange-300'},
+  {name: 'c#', color: 'orange-200'}
+]
+const frontend = [
+  {name: 'javascript', color: 'black', textColor: 'text-white'},
+  {name: 'typescript', color: 'slate-300'},
+  {name: 'vue.js', color: 'slate-700', textColor: 'text-white'},
+  {name: 'bootstrap', color: 'slate-700', textColor: 'text-white'},
+  {name: 'tailwind', color: 'slate-400'},
+]
+const database = [
+  {name: 'SQLServer', color: 'black', textColor: 'text-white'},
+  {name: 'PostgreSQL', color: 'slate-700', textColor: 'text-white'},
+  {name: 'SQLite', color: 'slate-300'},
+]
+</script>
