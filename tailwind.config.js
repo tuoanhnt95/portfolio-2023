c4ed970f3601a9799faf5bf90780efa0f72c33dc
Oanh Nguyen <myspamvn@yahoo.com>
2023-04-25 21:44:27 +0900

    feat: add Project components and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,html,vue}",
     "./src/App.vue",
-    './src/views/*.{html,js,vue}',
-    './src/components/*.{html,js,vue}',
+    './pages/**/*.{html,js,vue}',
+    './components/**/*.{html,js,vue}'
   ],
   theme: {
     extend: {},
-    screens: {
-      'xs': '414px',
-      ...defaultTheme.screens,
-    },
   },
   plugins: [],
 }
