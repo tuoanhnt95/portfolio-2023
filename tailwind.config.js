3206221839414a943845245f67d869d442b065c0
Oanh Nguyen <myspamvn@yahoo.com>
2023-04-25 21:49:28 +0900

    build: add customized Tailwind screensize xs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,html,vue}",
     "./src/App.vue",
-    './pages/**/*.{html,js,vue}',
-    './components/**/*.{html,js,vue}'
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '414px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
